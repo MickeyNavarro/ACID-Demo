cc000dc47b209baa0dafbc16e0857626e863ea07
Mickey Navarro <37810413+MickeyNavarro@users.noreply.github.com>
2019-02-16 20:18:58 -0700

    Update Tester.php

--- Tester.php
+++ Tester.php
@@ -5,6 +5,34 @@ ini_set ('display_errors', 1);
 ini_set ('display_startup_errors', 1);
 error_reporting(E_ALL);
 
+/* 
+//create a database connection
+$db = new Database(); 
+$conn = $db->getConnection(); 
+
+//create instances of each DAO 
+$checking = new CheckingAccountDataService($conn);
+$saving = new SavingAccountDataService($conn);
+
+//output the current balances
+$checkBalance = $checking->getBalance();
+$savingBalance = $saving->getBalance();
+echo "Current values:<br>";
+echo "Checking balance = " . $checkBalance . "<br>";
+echo "Saving balance = " . $savingBalance . "<br>";
+
+//uptdate the balances 
+echo "Take $100 from checking and put it into savings<br>";
+$checking->updateBalance($checkBalance - 100);
+$saving->updateBalance($savingBalance + 100);
+
+//output the new balances
+$checkBalance = $checking->getBalance();
+$savingBalance = $saving->getBalance();
+echo "Current values:<br>";
+echo "Checking balance = " . $checkBalance . "<br>";
+echo "Saving balance = " . $savingBalance . "<br>";
+*/
 
 //get the BankBusinessService 
 $service = new BankBusinessService(); 
